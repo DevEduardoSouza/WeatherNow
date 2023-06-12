efb9cdac02439804512d56879e02b254860c296a
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-12 18:34:41 -0300

    Mapeando as rotas do projeto

--- weathernow/src/main.jsx
+++ weathernow/src/main.jsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+            <Route path='/home' element={<Home/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>,
 )
