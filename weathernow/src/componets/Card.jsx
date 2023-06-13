2877ced1e931933643cfdd98f80e20d466bb36dd
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-13 11:47:53 -0300

    Terminando o css do card

--- weathernow/src/componets/Card.jsx
+++ weathernow/src/componets/Card.jsx
@@ -18,7 +18,7 @@ const card = () => {
             </div>
 
             <div className='climate-widget'>
-                <span> <BsFillSunFill /> </span>
+                <span className='icon'> <BsFillSunFill  className='largeIcon' /> </span>
                 <span className='temperature'>33°</span>
                 <span className='climate'>Sunny</span>
             </div>
@@ -26,15 +26,15 @@ const card = () => {
             <div className='daily-temperature-widget'>
                 <div className='today'>
                     <span>Today</span>
-                    <span>33°/25</span>
+                    <span>33°/ 25°</span>
                 </div>
                 <div className='today+1'>
                     <span>Wednesday</span>
-                    <span>33°/25</span>
+                    <span>33°/ 25°</span>
                 </div>
                 <div className='today+2'>
                     <span>Thursday</span>
-                    <span>33°/25</span>
+                    <span>33°/ 25°</span>
                 </div>
             </div>
 
