845d6152eed3da6a7d9082cc4ea5618e8c01b8a0
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-21 12:17:57 -0300

    Adicionado as cores de BG da home

--- weathernow/src/App.jsx
+++ weathernow/src/App.jsx
@@ -1,13 +1,11 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 
 function App() {
-  
-
   return (
     <>
       <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- weathernow/src/pages/Home.jsx
+++ weathernow/src/pages/Home.jsx
@@ -1,25 +1,20 @@
-import './Home.css'
-import React, { useEffect, useState } from 'react'
+import "./Home.css";
+import React, { useEffect, useState } from "react";
 
-import Card from '../componets/Card.jsx'
-
-
+import Card from "../componets/Card.jsx";
+import "./Home.css";
 
 const tempoURL = import.meta.env.VITE_API;
 const prevURL = import.meta.env.VITE_API_PREVISAO;
 const apiKey = import.meta.env.VITE_API_KEY;
 
-
 const Home = () => {
-
-
   const [tempoInfos, setTempoInfos] = useState([]);
   const [prevInfos, setPrevInfos] = useState([]);
 
-  const getInfoTempo = async (url) =>{
+  const getInfoTempo = async (url) => {
     const resposta = await fetch(url);
     const data = await resposta.json();
-
 
     const temperatura = data.data[0].temp;
     data.data[0].temp = parseInt(temperatura.toFixed());
@@ -27,33 +22,30 @@ const Home = () => {
 
     // console.log(data.data[0].city_name);
     // console.log(data.city_name);
-  }
+  };
 
-  const getPrevisoes = async (url) =>{
+  const getPrevisoes = async (url) => {
     const resposta = await fetch(url);
     const data = await resposta.json();
 
     setPrevInfos(data.data[0]);
     // console.log(data);
     // console.log(data.data[0].app_max_temp);
-  }
-  
+  };
 
-  useEffect(() => { 
-    const tempoAtualURL =`${tempoURL}?lang=pt&city=Irecê&${apiKey}&include=minutely`;
-    const prevAtualURL =`${prevURL}?lang=pt&city=Irecê&${apiKey}`;
+  useEffect(() => {
+    const tempoAtualURL = `${tempoURL}?lang=pt&city=Irecê&${apiKey}&include=minutely`;
+    const prevAtualURL = `${prevURL}?lang=pt&city=Irecê&${apiKey}`;
     getInfoTempo(tempoAtualURL);
     getPrevisoes(prevAtualURL);
   }, []);
 
-
-
-
   return (
-    <div className='cotainerHome'>
-      <Card tempoAtual={tempoInfos} prevTempo={prevInfos}/>
+    <div className="container-home">
+      {/* <Card tempoAtual={tempoInfos} prevTempo={prevInfos}/> */}
+      <div className="content-home"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
