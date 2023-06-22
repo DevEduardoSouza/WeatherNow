c7f680a419df19303a1c4546af1460357fe89353
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-22 16:35:41 -0300

    Adicionando um Navigate para a home

--- weathernow/src/main.jsx
+++ weathernow/src/main.jsx
@@ -1,22 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import App from "./App.jsx";
+import "./index.css";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-
     <BrowserRouter>
       <Routes>
-        <Route element={<App />}>
-            <Route path='/home' element={<Home/>} />
-        </Route>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
-    <App />
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
